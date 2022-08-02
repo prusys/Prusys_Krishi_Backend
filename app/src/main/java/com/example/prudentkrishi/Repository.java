5d0d4618d7e8cd6f166459b8a6aa634a01e0e3ad
Chakravardhan <raochakravavrdhan@gmail.com>
2022-08-02 21:18:55 +0530

    DAY 1

---
+++ app/src/main/java/com/example/prudentkrishi/Repository.java
@@ -0,0 +1,8 @@
+package com.example.prudentkrishi;
+import java.*;
+public class Repository  {
+Temperature t=new Temperature("28c","3mm","200f","10");
+
+
+
+}
---
+++ app/src/main/java/com/example/prudentkrishi/TempDetails.java
@@ -0,0 +1,16 @@
+package com.example.prudentkrishi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TempDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_details);
+
+
+    }
+}
